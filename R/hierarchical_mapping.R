89f97143804fb4d1ebb1e51cc67e00d037e334b1
amissarova <alsu.missarova.32@gmail.com>
2021-01-08 12:38:23 +0000

    update mapping functions so they return real celltype labels as well

--- R/hierarchical_mapping.R
+++ R/hierarchical_mapping.R
@@ -1,17 +1,18 @@
 
-#' MNN-based mapping in hierarchical fashion.
+#' MNN-based mapping in hierarchical fashion
 #'
 #' @param sce_reference SingleCellExperiment object representing reference scRNA-seq counts matrix containing celltype field.
 #' @param sce_query SingleCellExperiment object representing query scRNA-seq counts matrix containing celltype field. Cells and accordingly celltype labels from this ds would be assigned neighbors from sce_reference.
-#' @param genes Genes that are used for integration between sce_reference and sce_query
-#' @param ct_hierarchy Nested lists object representing celltype hierarchy
+#' @param genes Genes that are used for integration between sce_reference and sce_query.
 #' @param batch A field from colData of sce object that indicates which field is used as batch. If specified, PCA would be calculated using multiBatchPCA and pcs would be batch-corrected. Default=NULL that assumes no batch.
 #' @param cosineNorm A boolean specifying whether cosine normalization should be applied to logcounts. Default=TRUE.
-#' @param n.neigh Number of nearets neighbrs used for the integration. Default=10.
-#' @param nPC Number of PCs to be used. Default=NULL, and in this case all significant PCs (reflecting dependence between celltype and PC from ANOVA) will be included.
-#' @param p.thresh A P-value threshold to determine significance of each PC. Default=0.05.
-
-#' @return data.frame object, where every row corresponds to cell from sce_query. celltype_mapped field contains assigned from sce_reference celltype.
+#' @param n.neigh Positive scalar specifying number of nearest neighbors to be used in the integration. Default=10.
+#' @param nPC Number of PCs to be used for the integration between sce_reference and sce_query. Default=NULL, and in this case all significant PCs (reflecting dependence between celltype and PC from ANOVA) will be included.
+#' @param nPC.ct_hierarchy Number of PCs to be used in building celltype hierarchy. Default=NULL, and in this case all significant PCs (reflecting dependence between celltype and PC from ANOVA) will be included.
+#' @param p.thresh A P-value threshold to determine significance of each PC (for the integration between sce_reference and sce_query). Default=0.05.
+#' @param p.thresh.ct_hierarchy A P-value threshold to determine significance of each PC (for building the celltype hierarchy). Default=0.05.
+#'
+#' @return data.frame object, where every row corresponds to cell from sce_query. 'celltype' field contains actual celltype labels. 'celltype_mapped' field contains assigned from sce_reference celltype.
 #' @export
 #'
 #' @examples
@@ -25,23 +26,45 @@
 #' sce$cell = colnames(sce)
 #' sce$celltype = as.factor(sample(1:5, n_col, replace=TRUE))
 #' genes = c(1:20)
-#' ct_hierarchy = get_ct_hierarchy(sce , genes = genes, nPC = 10)
 #' sce_reference = sce[ , colnames(sce) %in% cells_reference]
 #' sce_query = sce[ , !colnames(sce) %in% cells_reference]
-#' out = hierarchical_mapping(sce_reference , sce_query , genes , ct_hierarchy, nPC=5)
+#' out = hierarchical_mapping(sce_reference , sce_query , genes, nPC = 5, nPC.ct_hierarchy=5)
+#'
+hierarchical_mapping = function( sce_reference , sce_query , genes , batch = NULL , cosineNorm = TRUE , n.neigh = 10 , nPC = NULL , nPC.ct_hierarchy = NULL, p.thresh = 0.05, p.thresh.ct_hierarchy = 0.05){
+  if (!.check_counts_matrix_correct(sce_reference) | !.check_counts_matrix_correct(sce_query)) {
+    stop()
+  } else {
+    if ( (sum(genes %in% rownames(sce_reference)) < length(genes)) | (sum(genes %in% rownames(sce_query)) < length(genes))){
+      stop("Either reference or query ds do not have some genes in their rownames.")
+    } else {
+      ct_hierarchy = get_ct_hierarchy(sce_reference , genes = genes , batch = batch, cosineNorm = cosineNorm , nPC = nPC.ct_hierarchy, p.thresh = p.thresh.ct_hierarchy)
 
-hierarchical_mapping = function( sce_reference , sce_query , genes, ct_hierarchy , batch = NULL , cosineNorm = TRUE , n.neigh = 10 , nPC = NULL , p.thresh = 0.05){
-  mapping = .mapping_single_split(sce_reference, sce_query, genes, ct_hierarchy, batch, cosineNorm, n.neigh, nPC, p.thresh)
+      # select genes entries and order
+      sce_reference = sce_reference[rownames(sce_reference) %in% genes , ]
+      sce_reference = sce_reference[order(rownames(sce_reference)),]
+      sce_query = sce_query[rownames(sce_query) %in% genes , ]
+      sce_query = sce_query[order(rownames(sce_query)),]
+
+      mapping = .hierarchical_mapping_intermediate(sce_reference, sce_query, ct_hierarchy, batch, cosineNorm, n.neigh, nPC, p.thresh)
+      meta = as.data.frame(colData(sce_query))
+      mapping = merge(mapping , meta[, c("cell" , "celltype")])
+      return(mapping)
+    }
+  }
+}
+
+
+.hierarchical_mapping_intermediate = function( sce_reference , sce_query, ct_hierarchy , batch = NULL , cosineNorm = TRUE , n.neigh = 10 , nPC = NULL , p.thresh = 0.05){
+  mapping = .mapping_single_split(sce_reference, sce_query, ct_hierarchy, batch, cosineNorm, n.neigh, nPC, p.thresh)
   mappings = lapply(1:2, function(i){
     if (!is(ct_hierarchy[[i]] , "list")){
       current_mapping = mapping[mapping$celltype_mapped == ct_hierarchy[[i]] , ]
       return(current_mapping)
-    }
-    else {
+    } else {
       current_sce_reference = sce_reference[, sce_reference$celltype %in% unlist( ct_hierarchy[[i]] ) ]
       current_sce_query = sce_query[, colnames(sce_query) %in% mapping$cell[mapping$celltype_mapped == i]]
       current_ct_hierarchy = ct_hierarchy[[i]]
-      return(hierarchical_mapping(current_sce_reference , current_sce_query , genes , current_ct_hierarchy, batch, cosineNorm, n.neigh, nPC, p.thresh))
+      return(.hierarchical_mapping_intermediate(current_sce_reference , current_sce_query, current_ct_hierarchy, batch, cosineNorm, n.neigh, nPC, p.thresh))
     }
   })
   mappings = do.call(rbind , mappings)
@@ -49,41 +72,32 @@ hierarchical_mapping = function( sce_reference , sce_query , genes, ct_hierarchy
 }
 
 
-.mapping_single_split = function(sce_reference , sce_query , genes , ct_hierarchy , batch = NULL , cosineNorm = TRUE , n.neigh = 10, nPC = NULL , p.thresh){
+.mapping_single_split = function(sce_reference, sce_query, ct_hierarchy , batch = NULL , cosineNorm = TRUE , n.neigh = 10, nPC = NULL , p.thresh){
   if (!.check_counts_matrix_correct(sce_reference) | !.check_counts_matrix_correct(sce_query)) {
     stop()
   } else {
-    if ( (sum(genes %in% rownames(sce_reference)) < length(genes)) | (sum(genes %in% rownames(sce_query)) < length(genes))){
-      stop("Either reference or query ds do not have some genes in their rownames.")
-    } else {
-      sce_reference = sce_reference[rownames(sce_reference) %in% genes , ]
-      sce_reference = sce_reference[order(rownames(sce_reference)),]
-      sce_query = sce_query[rownames(sce_query) %in% genes , ]
-      sce_query = sce_query[order(rownames(sce_query)),]
-
-      sce_reference = sce_reference[, sce_reference$celltype %in% unlist(ct_hierarchy)]
-      celltypes.1 = unlist(ct_hierarchy[[1]])
-      celltypes.2 = unlist(ct_hierarchy[[2]])
-      sce_reference$celltype = sapply(sce_reference$celltype , function(celltype) ifelse(celltype %in% celltypes.1 , 1 , 2) )
-      sce_query$celltype = sapply(sce_query$celltype , function(celltype){
-        if (celltype %in% celltypes.1){
-          return(1)
-        }
-        else if (celltype %in% celltypes.2){
-          return(2)
-        } else {
-          return(0)
-        }
-      })
-      mapping = mapping(sce_reference, sce_query, genes, batch, cosineNorm, n.neigh, nPC, p.thresh)
-      if (!is.list(ct_hierarchy[[1]])){
-        mapping$celltype_mapped[mapping$celltype_mapped == 1] = celltypes.1
+    sce_reference = sce_reference[, sce_reference$celltype %in% unlist(ct_hierarchy)]
+    celltypes.1 = unlist(ct_hierarchy[[1]])
+    celltypes.2 = unlist(ct_hierarchy[[2]])
+    sce_reference$celltype = sapply(sce_reference$celltype , function(celltype) ifelse(celltype %in% celltypes.1 , 1 , 2) )
+    sce_query$celltype = sapply(sce_query$celltype , function(celltype){
+      if (celltype %in% celltypes.1){
+        return(1)
       }
-      if (!is.list(ct_hierarchy[[2]])){
-        mapping$celltype_mapped[mapping$celltype_mapped == 2] = celltypes.2
+      else if (celltype %in% celltypes.2){
+        return(2)
+      } else {
+        return(0)
       }
-      return(mapping)
+    })
+    mapping = .mapping_intermediate(sce_reference, sce_query, batch, cosineNorm, n.neigh, nPC, p.thresh)
+    if (!is.list(ct_hierarchy[[1]])){
+      mapping$celltype_mapped[mapping$celltype_mapped == 1] = celltypes.1
     }
+    if (!is.list(ct_hierarchy[[2]])){
+      mapping$celltype_mapped[mapping$celltype_mapped == 2] = celltypes.2
+    }
+    return(mapping)
   }
 }
 
--- R/mapping.R
+++ R/mapping.R
@@ -1,3 +1,5 @@
+
+
 #' MNN-based mapping of celltypes
 #'
 #' @param sce_reference SingleCellExperiment object representing reference scRNA-seq counts matrix containing celltype field.
@@ -9,9 +11,11 @@
 #' @param nPC Number of PCs to be used. Default=NULL, and in this case all significant PCs (reflecting dependence between celltype and PC from ANOVA) will be included.
 #' @param p.thresh A P-value threshold to determine significance of each PC. Default=0.05.
 #'
-#' @return data.frame object, where every row corresponds to cell from sce_query. celltype_mapped field contains assigned from sce_reference celltype.
+#'
+#' @return data.frame object, where every row corresponds to cell from sce_query. 'celltype' field contains actual celltype labels. 'celltype_mapped' field contains assigned from sce_reference celltype.
 #' @export
 #' @importFrom BiocNeighbors queryKNN
+#'
 #' @examples
 #' require(SingleCellExperiment)
 #' n_row = 30000
@@ -26,41 +30,55 @@
 #' sce_query = sce[ , !colnames(sce) %in% cells_reference]
 #' genes = c(1:20)
 #' out = mapping(sce_reference , sce_query , genes, nPC=5)
-
-mapping = function(sce_reference , sce_query , genes , batch = NULL , cosineNorm = TRUE , n.neigh = 10 , nPC = NULL , p.thresh = 0.05){
+#'
+mapping = function(sce_reference , sce_query , genes , batch = NULL ,
+                   cosineNorm = TRUE , n.neigh = 10 , nPC = NULL ,
+                   p.thresh = 0.05) {
   if (!.check_counts_matrix_correct(sce_reference) | !.check_counts_matrix_correct(sce_query)) {
     stop()
   } else {
     if ( (sum(genes %in% rownames(sce_reference)) < length(genes)) | (sum(genes %in% rownames(sce_query)) < length(genes))){
       stop("Either reference or query ds do not have some genes in their rownames.")
     } else {
+      # select genes entries and order
       sce_reference = sce_reference[rownames(sce_reference) %in% genes , ]
       sce_reference = sce_reference[order(rownames(sce_reference)),]
       sce_query = sce_query[rownames(sce_query) %in% genes , ]
       sce_query = sce_query[order(rownames(sce_query)),]
-      sce = cbind(sce_reference , sce_query)
-      meta = as.data.frame(colData(sce))
 
-      # if only one celltype is present in the reference ds - assign it immediately.
-      if (length(unique(sce_reference$celltype)) == 1){
-        mapping = data.frame(cell = colnames(sce_query) , celltype_mapped = unique(sce_reference$celltype) )
-        mapping$celltype_mapped = as.character(mapping$celltype_mapped)
-      }
-      else {
-        pcs_corrected = .get_corrected_pcs(sce , genes , batch , cosineNorm)
-        relevant_pcs = .get_relevant_for_celltypes_pcs(pcs_corrected , sce_reference, nPC, p.thresh)
-        pcs_corrected = pcs_corrected[, relevant_pcs]
-
-        reference_cells = colnames(sce_reference)
-        query_cells = colnames(sce_query)
-        knns = suppressWarnings( queryKNN( pcs_corrected[reference_cells ,], pcs_corrected[query_cells ,], k = n.neigh, get.index = TRUE) )
-        cells_mapped = t( apply(knns$index, 1, function(x) reference_cells[x]) )
-        celltypes = t(apply(cells_mapped, 1, function(x) meta$celltype[match(x, meta$cell)]))
-        celltype_mapped = apply(celltypes, 1, function(x) .getmode(x, 1:length(x)))
-        mapping = data.frame(cell = query_cells , celltype_mapped = celltype_mapped)
-        mapping$celltype_mapped = as.character(mapping$celltype_mapped)
-      }
+      mapping = .mapping_intermediate(sce_reference, sce_query, batch, cosineNorm, n.neigh, nPC, p.thresh)
+      meta = as.data.frame(colData(sce_query))
+      mapping = merge(mapping , meta[, c("cell" , "celltype")])
       return(mapping)
     }
+  }
+}
+
+.mapping_intermediate = function(sce_reference , sce_query , batch = NULL , cosineNorm = TRUE , n.neigh = 10 , nPC = NULL , p.thresh = 0.05){
+  if (!.check_counts_matrix_correct(sce_reference) | !.check_counts_matrix_correct(sce_query)) {
+    stop()
+  } else {
+    sce = cbind(sce_reference , sce_query)
+    meta = as.data.frame(colData(sce))
+
+    # if only one celltype is present in the reference ds - assign it immediately.
+    if (length(unique(sce_reference$celltype)) == 1){
+      mapping = data.frame(cell = colnames(sce_query) , celltype_mapped = unique(sce_reference$celltype) )
+      mapping$celltype_mapped = as.character(mapping$celltype_mapped)
+    }
+    else {
+      pcs_corrected = .get_corrected_pcs(sce , genes , batch , cosineNorm)
+      relevant_pcs = .get_relevant_for_celltypes_pcs(pcs_corrected , sce_reference, nPC, p.thresh)
+      pcs_corrected = pcs_corrected[, relevant_pcs]
+      reference_cells = colnames(sce_reference)
+      query_cells = colnames(sce_query)
+      knns = suppressWarnings( queryKNN( pcs_corrected[reference_cells ,], pcs_corrected[query_cells ,], k = n.neigh, get.index = TRUE) )
+      cells_mapped = t( apply(knns$index, 1, function(x) reference_cells[x]) )
+      celltypes = t(apply(cells_mapped, 1, function(x) meta$celltype[match(x, meta$cell)]))
+      celltype_mapped = apply(celltypes, 1, function(x) .getmode(x, 1:length(x)))
+      mapping = data.frame(cell = query_cells , celltype_mapped = celltype_mapped)
+      mapping$celltype_mapped = as.character(mapping$celltype_mapped)
+    }
+    return(mapping)
   }
 }
