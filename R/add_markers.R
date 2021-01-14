c6fa199c180c5fcce351ae16444021419556b3d9
amissarova <alsu.missarova.32@gmail.com>
2021-01-14 11:03:28 +0000

    added 2 separate vars: one for cts to look within, one for cts to look for

--- R/add_markers.R
+++ R/add_markers.R
@@ -7,7 +7,7 @@
 #' @return A \code{data.frame} object with selected markers.
 #' @export
 #' @importFrom scran findMarkers
-#'
+#' @importFrom tibble rownames_to_column
 #' @examples
 #' require(SingleCellExperiment)
 #' n_row = 30000
@@ -18,18 +18,19 @@
 #' sce$cell = colnames(sce)
 #' sce$celltype = as.factor(sample(1:10, n_col, replace=TRUE))
 #' out = add_markers(sce)
-add_markers = function(sce, celltypes=NULL, Top=1){
+add_markers = function(sce, celltypes_distinguish = NULL, celltypes_add_markers = celltypes_distinguish, test = "t", Top=1){
   if (!.check_counts_matrix_correct(sce)) {
     stop()
   } else {
-    if (!is.null(celltypes)){
-      sce = sce[, sce$celltype %in% celltypes]
+    if (!is.null(celltypes_distinguish)){
+      sce = sce[, sce$celltype %in% celltypes_distinguish]
     }
-    markers = findMarkers(sce , groups=sce$celltype, direction = "up", test = "t", assay.type = "logcounts", pval.type="any")
-    markers.df = lapply(unique(sce$celltype), function(celltype){
+    markers = findMarkers(sce , groups=sce$celltype, direction = "up", test = test, assay.type = "logcounts", pval.type="any")
+    markers.df = lapply(celltypes_add_markers, function(celltype){
       current_markers = as.data.frame( markers[[which(names(markers) == celltype)]] )
       current_markers$celltype = celltype
-      return(current_markers[current_markers$Top <= Top , c("Top", "p.value", "FDR", "summary.logFC", "celltype")])
+      current_markers = rownames_to_column(current_markers , var = "gene")
+      return(current_markers[current_markers$Top <= Top , c("Top", "p.value", "FDR", "summary.logFC", "celltype" , "gene")])
     })
     markers.df = do.call(rbind , markers.df)
     return(markers.df)
