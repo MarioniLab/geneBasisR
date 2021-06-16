ced937afb270d549b2c53f118dfdb8ffcf8365be
amissarova <alsu.missarova.32@gmail.com>
2021-06-16 13:06:16 +0100

    updated no returns for NULL cases + msgs

--- R/evaluate_library.R
+++ R/evaluate_library.R
@@ -14,6 +14,8 @@
 #' @param return.gene_score_stat Boolean identifying whether stat on gene prediction score should be returned. Default return.gene_score_stat=TRUE.
 #' @param return.celltype_stat Boolean identifying whether stat on celltype mapping should be returned. Default return.celltype_stat=TRUE.
 #' @param verbose Boolean identifying whether intermediate print outputs should be returned. Default verbose=TRUE.
+#' @param neighs.all If not NULL (NULL is default), contains information about True kNN-graph (for each cell - ordered neighbors and distances). Useful to have a priori if cell score will be recalculated multiple times.
+#' @param gene_stat_all If not NULL (NULL is default), gene_stat_all is pre-calculated stat for True graph. This is useful if this variable will be used re-used multiple times.
 #' @param ... Additional parameters
 #'
 #' @return
@@ -33,7 +35,8 @@
 #'
 evaluate_library = function(sce, genes.selection, genes.all = rownames(sce), batch = NULL,
                             library.size_type = "single" , n_genes.step = 10,
-                            return.cell_score_stat = T, return.gene_score_stat = T, return.celltype_stat = T, verbose = TRUE, ...){
+                            return.cell_score_stat = T, return.gene_score_stat = T, return.celltype_stat = T, verbose = TRUE,
+                            neighs.all = NULL, gene_stat_all = NULL, ...){
   sce = .prepare_sce(sce)
   args = c(as.list(environment()), list(...))
   out = .general_check_arguments(args) & .check_batch(sce , batch) & .check_genes_in_sce(sce , genes.selection) & .check_genes_in_sce(sce, genes.all)
@@ -47,19 +50,29 @@ evaluate_library = function(sce, genes.selection, genes.all = rownames(sce), bat
     n_genes.grid = c( seq(n_genes.step , length(genes.selection) , n_genes.step) , length(genes.selection))
     n_genes.grid = sort(unique(n_genes.grid))
   }
+
   if (sum(return.cell_score_stat, return.gene_score_stat, return.celltype_stat) == 0){
     stop("Select at least one stat to return.")
     return(F)
   }
   else {
-    out = list()
-    names(out) = c()
+    final_stat = list()
+    names(final_stat) = c()
 
+
+    #### cell score stat
     if (return.cell_score_stat){
       if (verbose){
         cat("Calculating cell neighborhood preservation scores.\n")
       }
-      neighs.all = suppressWarnings( get_z_scaled_distances(sce , genes.all = genes.all , batch = batch, ...) )
+      if (!is.null(neighs.all)){
+        if (!is.null(batch)){
+          out = .check_neighs.all_multipleBatches(sce , batch = batch , neighs.all = neighs.all)
+        }
+      }
+      else {
+        neighs.all = suppressWarnings( get_z_scaled_distances(sce , genes.all = genes.all , batch = batch, ...) )
+      }
       cell_score_stat = lapply(n_genes.grid, function(n_genes){
         current.stat = suppressWarnings( get_neighborhood_preservation_scores(sce, neighs.all = neighs.all,  genes.all = genes.all,
                                                                        genes.selection = genes.selection[1:n_genes], batch = batch, check_args = FALSE, ...) )
@@ -71,20 +84,22 @@ evaluate_library = function(sce, genes.selection, genes.all = rownames(sce), bat
       })
       cell_score_stat = do.call(rbind , cell_score_stat)
       cell_score_stat$n_genes = factor(cell_score_stat$n_genes, levels = n_genes.grid)
-      out[[length(out) + 1]] = cell_score_stat
-      names(out)[length(out)] = "cell_score_stat"
+      final_stat[[length(final_stat) + 1]] = cell_score_stat
+      names(final_stat)[length(final_stat)] = "cell_score_stat"
       if (verbose){
         cat("Finished calculation of cell neighborhood preservation scores.\n")
       }
     }
 
-
+    #### gene score stat
     if (return.gene_score_stat){
       if (verbose){
         cat("Calculating gene prediction scores.\n")
       }
-      gene_stat_all = suppressWarnings( get_gene_correlation_scores(sce, genes.all, batch = batch, ...) )
-      colnames(gene_stat_all) = c("gene" , "corr_all")
+      if (is.null(gene_stat_all)){
+        gene_stat_all = suppressWarnings( get_gene_correlation_scores(sce, genes.all, batch = batch, ...) )
+        colnames(gene_stat_all) = c("gene" , "corr_all")
+      }
       gene_score_stat = lapply(n_genes.grid, function(n_genes){
         current.stat = suppressWarnings( get_gene_prediction_scores(sce, genes.selection[1:n_genes], genes.all = genes.all, batch = batch, gene_stat_all = gene_stat_all, check_args = FALSE, ...) )
         if (!is.null(current.stat)){
@@ -98,39 +113,45 @@ evaluate_library = function(sce, genes.selection, genes.all = rownames(sce), bat
       gene_score_stat = do.call(rbind , gene_score_stat)
       if (!is.null(gene_score_stat)){
         gene_score_stat$n_genes = factor(gene_score_stat$n_genes, levels = n_genes.grid)
+        final_stat[[length(final_stat) + 1]] = gene_score_stat
+        names(final_stat)[length(final_stat)] = "gene_score_stat"
       }
-      else {
-        gene_score_stat = NaN
-      }
-      out[[length(out) + 1]] = gene_score_stat
-      names(out)[length(out)] = "gene_score_stat"
       if (verbose){
         cat("Finished calculation of gene prediction scores.\n")
       }
     }
 
 
+    ### celltype stat
     if (return.celltype_stat){
       if (verbose){
         cat("Calculating accuracy of cell type mappings.\n")
       }
       celltype_stat = lapply(n_genes.grid, function(n_genes){
         current.stat = get_celltype_mapping(sce , genes.selection[1:n_genes] , batch = batch, return.stat = T, check_args = FALSE, ...)
-        current.stat = current.stat$stat
-        current.stat$n_genes = n_genes
+        if (!is.null(current.stat)){
+          current.stat = current.stat$stat
+          current.stat$n_genes = n_genes
+        }
         if (verbose){
           cat(paste("Finished for the selection of" , n_genes , "genes.\n"))
         }
         return(current.stat)
       })
       celltype_stat = do.call(rbind , celltype_stat)
-      celltype_stat$n_genes = factor(celltype_stat$n_genes, levels = n_genes.grid)
-      out[[length(out) + 1]] = celltype_stat
-      names(out)[length(out)] = "celltype_stat"
+      if (!is.null(celltype_stat)){
+        celltype_stat$n_genes = factor(celltype_stat$n_genes, levels = n_genes.grid)
+        final_stat[[length(final_stat) + 1]] = celltype_stat
+        names(final_stat)[length(final_stat)] = "celltype_stat"
+      }
+      final_stat[[length(final_stat) + 1]] = celltype_stat
+      names(final_stat)[length(final_stat)] = "celltype_stat"
       if (verbose){
         cat("Finished calculation of accuracy of cell type mappings.\n")
       }
     }
-    return(out)
+    return(final_stat)
   }
 }
+
+
