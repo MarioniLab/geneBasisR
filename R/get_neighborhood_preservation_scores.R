c6d7d14d1ddb8d885c9602fadff6c0b6e4978901
amissarova <alsu.missarova.32@gmail.com>
2021-07-07 17:52:06 +0100

    add approx mean dist option for big ds

--- R/get_neighborhood_preservation_scores.R
+++ R/get_neighborhood_preservation_scores.R
@@ -13,6 +13,7 @@
 #'
 #' @return data.frame, each row corresponds to cell from counts matrix, contains field cell_score = cell neighborhood preservation score
 #' @export
+#' @importFrom Rfast dista
 #'
 #' @examples
 #' require(SingleCellExperiment)
@@ -38,59 +39,20 @@ get_neighborhood_preservation_scores = function(sce, neighs.all_stat = NULL, gen
       out = .general_check_arguments(args) & .check_batch(sce , batch) & .check_genes_in_sce(sce , genes.all) & .check_genes_in_sce(sce , genes.selection)
     }
   }
-  if (is.null(batch)){
-    score = .get_neighborhood_preservation_scores_single_batch(sce , neighs.all_stat = neighs.all_stat, genes.all = genes.all,
-                                                                           genes.selection = genes.selection, n.neigh = n.neigh ,
-                                                                           nPC.all = nPC.all , nPC.selection = nPC.selection)
-    return(score)
-  }
-  else {
-    if (is.null(neighs.all_stat)){
-      neighs.all_stat = get_neighs_all_stat(sce , genes.all = genes.all , batch = batch, n.neigh = n.neigh , nPC.all = nPC.all)
-    }
-    meta = as.data.frame(colData(sce))
-    batchFactor = factor(meta[, colnames(meta) == batch])
-    score = lapply(unique(batchFactor) , function(current.batch){
-      print(current.batch)
-      idx = which(batchFactor == current.batch)
-      current.sce = sce[, idx]
-      current.neighs.all_stat = neighs.all_stat[[which(names(neighs.all_stat) == current.batch)]]
-      current.score = .get_neighborhood_preservation_scores_single_batch(current.sce , neighs.all_stat = current.neighs.all_stat,
-                                                                                     genes.all = genes.all ,
-                                                                                     genes.selection = genes.selection, n.neigh = n.neigh ,
-                                                                                     nPC.all = nPC.all, nPC.selection = nPC.selection)
-      return(current.score)
-    })
-    score = do.call(rbind, score)
-    return(score)
-  }
-}
-
-
-#' @import Rfast
-#' @importFrom irlba prcomp_irlba
-.get_neighborhood_preservation_scores_single_batch = function(sce, neighs.all_stat = NULL, genes.all = rownames(sce),
-                                                                          genes.selection, n.neigh = 5, nPC.all = 50, nPC.selection = NULL){
-  set.seed(32)
-  sce = sce[genes.all , ]
-
   if (is.null(neighs.all_stat)){
-    neighs.all_stat = get_neighs_all_stat(sce , genes.all = genes.all , batch = NULL, n.neigh = n.neigh , nPC.all = nPC.all)
+    neighs.all_stat = get_neighs_all_stat(sce , genes.all = genes.all , batch = batch, n.neigh = n.neigh , nPC.all = nPC.all)
   }
-
   counts = neighs.all_stat$counts
   neighs.all = neighs.all_stat$neighs.all
   mean_dist = neighs.all_stat$mean_dist
-
-
-  neighs.compare = .get_mapping(sce , genes = genes.selection, batch = NULL, n.neigh = n.neigh, nPC = nPC.selection)
-
+  neighs.compare = .get_mapping(sce , genes = genes.selection, batch = batch, n.neigh = n.neigh, nPC = nPC.selection)
+  # order
   counts = counts[order(rownames(counts)),]
   neighs.all = neighs.all[order(rownames(neighs.all)),]
   neighs.compare = neighs.compare[order(rownames(neighs.compare)),]
   mean_dist = mean_dist[order(names(mean_dist))]
 
-
+  # get score
   score = lapply(1:nrow(counts), function(i){
     dist_neighs.all_coords = median(dista(t(counts[i, ]), counts[neighs.all[i,], ]))
     dist_neighs.compare_coords = median(dista(t(counts[i, ]), counts[neighs.compare[i,], ]))
@@ -104,19 +66,22 @@ get_neighborhood_preservation_scores = function(sce, neighs.all_stat = NULL, gen
 }
 
 
+
+
 #' get_neighs.all_stat
 #' @param sce SingleCellExperiment object containing gene counts matrix.
 #' @param genes.all String specifying genes to be used for construction of True kNN-graph.
 #' @param batch Name of the field in colData(sce) to specify batch. Default batch=NULL if no batch is applied.
 #' @param n.neigh Positive integer > 1, specifying number of neighbors to use for kNN-graph. Default n.neigh=5.
 #' @param nPC.all Scalar specifying number of PCs to use for construction of True kNN-graph. Default nPC.all=50.
+#' @param option String specifying how average distance for each cell should be calculated. If = 'exact', all other cells are taken into account. If = 'approx', the random subset of 10000 cells will be used. Approx recommended for big datasets.
 #' @param ... Additional arguments.
 #'
 #' @return kNN-graph with assigned neighbors and corresponding z-scored distances.
 #' @export
+#' @importFrom paleotree reverseList
 #'
-#'
-get_neighs_all_stat = function(sce , genes.all = rownames(sce) , batch = NULL, n.neigh = 5 , nPC.all = 50){
+get_neighs_all_stat = function(sce , genes.all = rownames(sce) , batch = NULL, n.neigh = 5 , nPC.all = 50 , option = "approx"){
   if (is.null(batch)){
     out = .get_neighs_all_stat_single_batch(sce , genes.all = genes.all , n.neigh = n.neigh , nPC.all = nPC.all)
     return(out)
@@ -127,9 +92,13 @@ get_neighs_all_stat = function(sce , genes.all = rownames(sce) , batch = NULL, n
     neighs.all_stat = lapply(unique(batchFactor) , function(current.batch){
       idx = which(batchFactor == current.batch)
       current.sce = sce[, idx]
-      out =  .get_neighs_all_stat_single_batch(current.sce , genes.all = genes.all , n.neigh = n.neigh , nPC.all = nPC.all)
+      out =  .get_neighs_all_stat_single_batch(current.sce , genes.all = genes.all , n.neigh = n.neigh , nPC.all = nPC.all, option = option)
     })
-    names(neighs.all_stat) = unique(batchFactor)
+    neighs.all_stat = reverseList(neighs.all_stat)
+    counts = do.call(rbind , neighs.all_stat$counts)
+    neighs.all = do.call(rbind , neighs.all_stat$neighs.all)
+    mean_dist = do.call(rbind, neighs.all_stat$mean_dist)
+    neighs.all_stat = list(counts = counts, neighs.all = neighs.all , mean_dist = mean_dist)
     return(neighs.all_stat)
   }
 }
@@ -139,7 +108,7 @@ get_neighs_all_stat = function(sce , genes.all = rownames(sce) , batch = NULL, n
 #' @import Matrix
 #' @importFrom Rfast dista
 #'
-.get_neighs_all_stat_single_batch = function(sce , genes.all = rownames(sce) , n.neigh = 5 , nPC.all = 50){
+.get_neighs_all_stat_single_batch = function(sce , genes.all = rownames(sce) , n.neigh = 5 , nPC.all = 50 , option = "approx"){
   set.seed(32)
   sce = sce[genes.all, ]
   res = tryCatch(
@@ -155,17 +124,27 @@ get_neighs_all_stat = function(sce , genes.all = rownames(sce) , batch = NULL, n
       neighs.all = .assign_neighbors(counts , reference_cells = colnames(sce), query_cells = colnames(sce), n.neigh = n.neigh)
 
       # get mean
-      mean_dist = sapply(rownames(counts), function(cell){
-        out = mean(dista(t(counts[cell, ]), counts[setdiff(rownames(counts), cell) , ]))
-        return(out)
-      })
+      if (option == "exact"){
+        denominator = nrow(counts) - 1
+        mean_dist = sapply(rownames(counts), function(cell){
+          out = sum(dista(t(counts[cell, ]), counts))/denominator
+          return(out)
+        })
+      }
+      else if (option == "approx"){
+        cells_random = sample( rownames(counts) , min(10000, (nrow(counts)-1)) )
+        mean_dist = sapply(rownames(counts), function(cell){
+          out = mean(dista(t(counts[cell, ]), counts[setdiff(cells_random , cell) , ] ))
+          return(out)
+        })
+      }
       names(mean_dist) = rownames(counts)
 
-      out = list(counts = counts, neighs.all = neighs.all, mean_dist = mean_dist)
-      return(out)
+      list(counts = counts, neighs.all = neighs.all, mean_dist = mean_dist)
+      #return(out)
     },
     error = function(dummy){
-      message("Count matrix is too big - we will be working with sparse matrices.")
+      message("test")
       counts = Matrix::t(logcounts(sce))
       if (!is.null(nPC.all)){
         pcs = suppressWarnings( prcomp_irlba(counts , n = min(nPC.all, (nrow(counts)-1) , (ncol(counts) - 1))) )
@@ -177,17 +156,27 @@ get_neighs_all_stat = function(sce , genes.all = rownames(sce) , batch = NULL, n
       neighs.all = .assign_neighbors(counts , reference_cells = colnames(sce), query_cells = colnames(sce), n.neigh = n.neigh)
 
       # get mean
-      mean_dist = sapply(rownames(counts), function(cell){
-        out = mean(dista(t(counts[cell, ]), counts[setdiff(rownames(counts), cell) , ]))
-        return(out)
-      })
+      if (option == "exact"){
+        denominator = nrow(counts) - 1
+        mean_dist = sapply(rownames(counts), function(cell){
+          out = sum(dista(t(counts[cell, ]), counts))/denominator
+          return(out)
+        })
+      }
+      else if (option == "approx"){
+        cells_random = sample( rownames(counts) , min(10000, (nrow(counts)-1)) )
+        mean_dist = sapply(rownames(counts), function(cell){
+          out = mean(dista(t(counts[cell, ]), counts[setdiff(cells_random , cell) , ] ))
+          return(out)
+        })
+      }
       names(mean_dist) = rownames(counts)
 
       out = list(counts = counts, neighs.all = neighs.all, mean_dist = mean_dist)
       return(out)
     },
     error = function(dump){
-      message("Either memory cupped or features you selected can not be used for pca")
+      message("Either memory cupped or features you selected can not be used for pca. Try downsampling, smaller n.neigh or smaller nPC.all.")
       return(NULL)
     }
   )
