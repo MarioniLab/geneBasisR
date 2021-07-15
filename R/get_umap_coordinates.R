4409cc7ce6dad064b4471c770c609aeec1cfcf55
amissarova <alsu.missarova.32@gmail.com>
2021-07-15 14:23:52 +0100

    actively move counts to list

--- R/get_umap_coordinates.R
+++ R/get_umap_coordinates.R
@@ -8,11 +8,12 @@
 #' @param batch Name of the field in colData(sce) to specify batch. Default batch=NULL if no batch is applied.
 #' @param nPC Scalar specifying number of PCs to use. Default = length(genes)-1
 #'
-#' @return Return data.frame, where UMAP-coordinates are assigned as x and y.
+#' @return data.frame, where UMAP-coordinates are assigned as x and y.
 #' @export
+#' @import SingleCellExperiment
+#' @import batchelor
 #' @importFrom uwot umap
 #' @importFrom irlba prcomp_irlba
-#' @import batchelor
 #' @importFrom tibble rownames_to_column
 #'
 #' @examples
@@ -28,7 +29,7 @@
 get_umap_coordinates = function(sce, genes, batch = NULL, nPC = length(genes) - 1){
   set.seed(32)
   sce = sce[genes, ]
-  counts = as.matrix( logcounts(sce))
+  counts = as.matrix(logcounts(sce))
   meta = as.data.frame(colData(sce))
   if (is.null(batch)){
     pcs = suppressWarnings( prcomp_irlba(t(counts) , n = min(nPC, (nrow(counts)-1) , (ncol(counts) - 1))) )
@@ -39,7 +40,7 @@ get_umap_coordinates = function(sce, genes, batch = NULL, nPC = length(genes) - 
     meta = as.data.frame(colData(sce))
     batchFactor = factor(meta[, colnames(meta) == batch])
     counts = multiBatchPCA(counts , batch = batchFactor , d = nPC)
-    counts = do.call(reducedMNN , counts)
+    counts = do.call(reducedMNN , as.list(counts))
     counts = counts$corrected
   }
   # get umap-coordinates
@@ -47,6 +48,6 @@ get_umap_coordinates = function(sce, genes, batch = NULL, nPC = length(genes) - 
   rownames(umaps) = rownames(counts)
   colnames(umaps) = c("x" , "y")
   umaps = rownames_to_column(umaps, var = "cell")
-  return(umaps)
+  return(counts)
 }
 
